19d110fc7374ffe19a5e5bfb50dcd2542fef8a76
skooter500 <skooter500@gmail.com>
2026-03-01 12:41:12 +0000

    x

--- hands/hand.gd
+++ hands/hand.gd
@@ -5,9 +5,11 @@ var gesture:String
 
 func _on_hand_pose_detector_pose_started(p_name: String) -> void:
 	gesture = p_name
+	$label.text = gesture
 	pass # Replace with function body.
 
 
 func _on_hand_pose_detector_pose_ended(p_name: String) -> void:
 	gesture = ""
+	$label.text = "none"
 	pass # Replace with function body.
